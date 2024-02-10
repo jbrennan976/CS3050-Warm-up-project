4275bdba8aa1576e3139e100dde620757e44429b
jbrennan976 <jbrennan9763@gmail.com>
2024-02-10 16:50:16 -0500

    Fixed file path

--- code/config.py
+++ code/config.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 def initFirestone():
-    cred = credentials.Certificate('team6Key.json')
+    cred = credentials.Certificate('../data/team6Key.json')
     app = firebase_admin.initialize_app(cred)
     db = firestore.client()
