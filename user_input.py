99db9a6bedfde992dc163caa0fe95527b0dd057c
Alex Stute <stute5000@gmail.com>
2024-02-10 15:08:02 -0500

    created base functions for interpreting user input

---
+++ user_input.py
@@ -0,0 +1,34 @@
+def interpret_field(field):
+    match field:
+        case 'Rank':
+            pass
+
+        case 'Artist':
+            pass
+
+        case 'Title':
+            pass
+
+        case 'Features':
+            pass
+    
+
+def interpret_cmd(command):
+    match command:
+        case 'Help':
+            pass
+
+        case 'asc':
+            pass
+
+        case 'is':
+            pass
+
+        case '<':
+            pass
+
+        case '>':
+            pass
+
+        case 'of':
+            pass
