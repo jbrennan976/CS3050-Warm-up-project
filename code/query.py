5d238ee4dc18941fb27c40c9b5ceaae38b603baa
arimenezes11 <44853139+arimenezes11@users.noreply.github.com>
2024-02-14 17:24:02 -0500

    created fleshed out query function

--- code/query.py
+++ code/query.py
@@ -1,14 +1,27 @@
-import Parser
-import config
+from google.cloud import firestore
 from Parser import Query
-
+import config
 db = config.initFirestone()
 song_ref = db.collection("song_rankings")
 
-example_query = Query(5, None, None, None, None, False)
-
 def query_db(query):
-    example_query.Lower_Rank
-    print (example_query.Lower_Rank)
+    req = song_ref.where("Rank", '>=', query.Lower_Rank).where("Rank", '<=', query.Upper_Rank)
+    if query.Artist is not None:
+        req = req.where('Artist', '==', query.Artist)
+    if query.Title is not None:
+        req = req.where('Song', '==', query.Title)
+    if query.Features:
+        req = req.where("has_feature", "==", True)
+    if not query.Desc:
+        req = req.order_by('Rank', direction=firestore.Query.ASCENDING)
 
-query_db(example_query)
+    res = []
+    data = req.get()
+    for e in data:
+        res.append(e.to_dict())
+    return res
+
+
+if __name__ == "__main__":
+    ex_q = query_db(Query(1, 40, None, "Memories", True, False))
+    print(ex_q)
