88f56a929b2525a900fae5c8bd0e86b8235a6540
Izzy Haase-Puissant <ahaasepu@uvm.edu>
2024-02-14 19:48:10 -0500

    Rounded out main

--- code/main.py
+++ code/main.py
@@ -6,8 +6,12 @@ def main():
     print("Enter queries below. Enter 'help' for a list of commands")
     # This loop will run until the user enters 'exit'
     user_input = ''
+    results = []
+
     while user_input.find("exit") == -1:
+        results.clear()
         user_input = input('?: ')
+        print("\n")
         send_to_parser = []
         send_to_parser = user_input.split(',')
         send_to_query = interpret_query(send_to_parser)
@@ -15,5 +19,7 @@ def main():
             results = query_db(send_to_query)
             pass
         for dict in results:
-            print(dict)
+            print(dict,"\n")
     return
+
+main()
